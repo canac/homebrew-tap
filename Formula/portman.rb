f01e286b110e99fff9f1391fb678e044f9288380
Caleb Cox <canac@users.noreply.github.com>
2022-03-22 21:19:42 -0500

    portman 0.1.0
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #3.
    
    Signed-off-by: Caleb Cox <3740187+canac@users.noreply.github.com>

--- Formula/portman.rb
+++ Formula/portman.rb
@@ -1,8 +1,8 @@
 class Portman < Formula
   desc "Local port manager"
   homepage "https://github.com/canac/portman"
-  url "https://github.com/canac/portman/archive/v0.0.4.tar.gz"
-  sha256 "4b9b5f82d90d929088ca668be7c95dde3d8e3b229fd4a5a8523fcd640a335931"
+  url "https://github.com/canac/portman/archive/v0.1.0.tar.gz"
+  sha256 "03b79142fbd5c2211170d822b07983ab13ecaf9fb86db21f43913f53c612866a"
   license "MIT"
 
   bottle do
