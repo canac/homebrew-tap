981740cda97882fdd0322cf2618e0f15c5340149
Caleb Cox <3740187+canac@users.noreply.github.com>
2022-07-10 22:50:01 +0000

    mailbox: add 0.1.3 bottle.

--- Formula/mailbox.rb
+++ Formula/mailbox.rb
@@ -5,6 +5,12 @@ class Mailbox < Formula
   sha256 "0ec3e7f843952dacc416185fc734151126aba73129b6b9a7ec3e605af58c1ce1"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/canac/homebrew-tap/releases/download/mailbox-0.1.3"
+    sha256 cellar: :any_skip_relocation, big_sur:      "e47e3b01a84ab080c54dfe212596887c5e1382d708fd934950d1381a8428402b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4355496d30079c78825d5ff42f1a1c9bbf81518444258ebe02edc363f4844a4d"
+  end
+
   depends_on "rust" => :build
 
   def install
