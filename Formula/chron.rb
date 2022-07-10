335271e39b6b2ba28fa300b2206e7015f1cdbb8c
Caleb Cox <canac@users.noreply.github.com>
2022-07-10 18:28:31 -0500

    chron 0.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/chron.rb
+++ Formula/chron.rb
@@ -1,8 +1,8 @@
 class Chron < Formula
   desc "Easily run scripts on a schedule"
   homepage "https://github.com/canac/chron"
-  url "https://github.com/canac/chron/archive/v0.1.0.tar.gz"
-  sha256 "98124249c0a760ac2a1ed9bc4a0ed7037b92a073a119ac0c7d6d30641849a90c"
+  url "https://github.com/canac/chron/archive/v0.1.1.tar.gz"
+  sha256 "2598cca4d407e047edf84a4fd47fbdbcb57321537e1ec4bb5227d542107adfd3"
   license "MIT"
 
   depends_on "rust" => :build
