fcfa5a5caf852fc58357fc4a113c1eb12a0721ec
Caleb Cox <3740187+canac@users.noreply.github.com>
2022-07-11 00:46:17 +0000

    chron: update 0.1.2 bottle.

--- Formula/chron.rb
+++ Formula/chron.rb
@@ -6,9 +6,9 @@ class Chron < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/canac/homebrew-tap/releases/download/chron-0.1.1"
-    sha256 cellar: :any_skip_relocation, big_sur:      "b9339f2dc605e7044dd3f970db79e4d0f0c1940a5b1930377e928d0fb5083f2f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "40f1aa64596f41e30ef3f3fd92ed42edd2deffa81354117165cc21de08c6c86e"
+    root_url "https://github.com/canac/homebrew-tap/releases/download/chron-0.1.2"
+    sha256 cellar: :any_skip_relocation, big_sur:      "78eae8cb93bac2db6dbb9e647de5a7d1c2b6b1827774b51fc36de465316c5eb6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "84db2ac913e0f7ff5a1707e42d4d679b3acb0958e9c2fdc0f2df2274ef5a8180"
   end
 
   depends_on "rust" => :build
