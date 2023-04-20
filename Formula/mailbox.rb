1340f45da4fb024e418b914b426d405b9ed0c2a8
Caleb Cox <3740187+canac@users.noreply.github.com>
2023-04-20 14:48:09 +0000

    mailbox: update 0.6.1 bottle.

--- Formula/mailbox.rb
+++ Formula/mailbox.rb
@@ -6,9 +6,9 @@ class Mailbox < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/canac/homebrew-tap/releases/download/mailbox-0.6.0"
-    sha256 cellar: :any_skip_relocation, monterey:     "a2a046e1ba76e4c5e327a6a6f9dfbbed88a72a1a9858e6091ec0595913c11f92"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "dc53d2cff230a98f10c1ff3264625e191bae164c58cfca2cbcd9189e389eb57d"
+    root_url "https://github.com/canac/homebrew-tap/releases/download/mailbox-0.6.1"
+    sha256 cellar: :any_skip_relocation, monterey:     "988b6111d7e73de44284be889d358e45479af84f89c42d235ad6939d94041f3e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1602434a12dcf279a29cda662febf141a8e445d755939dba91983adf473f5bae"
   end
 
   depends_on "rust" => :build
