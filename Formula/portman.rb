ad1d95021e979e802c4a6a5655fd241fb4bf5605
Caleb Cox <3740187+canac@users.noreply.github.com>
2023-08-15 18:52:37 +0000

    portman: update 0.4.0 bottle.

--- Formula/portman.rb
+++ Formula/portman.rb
@@ -6,9 +6,9 @@ class Portman < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/canac/homebrew-tap/releases/download/portman-0.3.4"
-    sha256 cellar: :any_skip_relocation, monterey:     "282d6f3ac95f8756d176ca57665533e11deabac6ad020b7177bbcb3812767c08"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "62333a72fc09e623bd44d74a21a90c0a957149894b42883147edd7e4a68eb2c1"
+    root_url "https://github.com/canac/homebrew-tap/releases/download/portman-0.4.0"
+    sha256 cellar: :any_skip_relocation, monterey:     "620e188f64679d727f47558859a8fc465ebec2ac29faa20344f447f60625f972"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5c65eedad82b7c556ea2580b0bc7099236c4f1e5ccf568b7125014e81c329934"
   end
 
   depends_on "rust" => :build
