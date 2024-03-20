f15aa97cd1020b74882833031345b2f165d08530
Caleb Cox <3740187+canac@users.noreply.github.com>
2024-03-20 02:39:45 +0000

    portman: update 0.6.1 bottle.

--- Formula/portman.rb
+++ Formula/portman.rb
@@ -6,11 +6,11 @@ class Portman < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/canac/homebrew-tap/releases/download/portman-0.6.0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "9442d2540af76dace0dd5c9a6d67da9da87c6c1a9c27231b3177607ef088077a"
-    sha256 cellar: :any_skip_relocation, ventura:      "bf267dd95c6add78b8108d559ea08a0f68ce65844da17942ef4a17bc9771337b"
-    sha256 cellar: :any_skip_relocation, monterey:     "f57930ba9f40a5fe2c23378861c212052e832076bc931f3ea81b596582cbbf51"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "f58cdbe4c521620799a6516abf0b40c42514cb97538dfa18a69231e67fec77a6"
+    root_url "https://github.com/canac/homebrew-tap/releases/download/portman-0.6.1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "5234872fd7eec26f4bea6e6c1e3a01d8e6560a5d98970b3ee153ca5b9eedb2c9"
+    sha256 cellar: :any_skip_relocation, ventura:      "65bfb6684cd3bfa56584d62e714eee63640770864141c66353106903eaabcc4f"
+    sha256 cellar: :any_skip_relocation, monterey:     "b8611f3002480c6b255ce4ea26b968bed7748b5a06f486fab026957b1c96c7fe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8b0852d83f1ffadffeb7479980c58aa786c21d58f2707e003e4ea72ee18017d2"
   end
 
   depends_on "rust" => :build
