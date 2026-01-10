d35810935fbd409fed8895b5054f4c53dc8e4bfc
Caleb Cox <canac@users.noreply.github.com>
2026-01-10 09:43:40 -0600

    Update mailbox.rb
    Closes #76.
    
    Signed-off-by: Caleb Cox <3740187+canac@users.noreply.github.com>

--- Formula/mailbox.rb
+++ Formula/mailbox.rb
@@ -4,6 +4,7 @@ class Mailbox < Formula
   url "https://github.com/canac/mailbox/archive/refs/tags/v0.8.6.tar.gz"
   sha256 "4e40c495eb40ec0ea37bf12174aa3e39534e63e2d5768d901e3ab7e16f95ec00"
   license "MIT"
+  revision 1
 
   bottle do
     root_url "https://github.com/canac/homebrew-tap/releases/download/mailbox-0.8.6"
