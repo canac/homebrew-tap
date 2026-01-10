1f625ebd7b7f7ce68ab29c0e094df3a2a772a082
Caleb Cox <3740187+canac@users.noreply.github.com>
2026-01-10 16:06:17 +0000

    kaw: update 0.1.4_1 bottle.

--- Formula/kaw.rb
+++ Formula/kaw.rb
@@ -7,11 +7,11 @@ class Kaw < Formula
   revision 1
 
   bottle do
-    root_url "https://github.com/canac/homebrew-tap/releases/download/kaw-0.1.4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "266527526c164d6617bc568ef5a26173720cde47bc8a9540c6760994571abdb3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ad11bcc9ee8adfe53875cf0ad315e7bc070780e2da9fc03db15d17971ddb0240"
-    sha256 cellar: :any_skip_relocation, ventura:       "c9597d88309af17512c1d2e1dc97408ad0cef5885515e6090bbe356f3bc67705"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bca68bde8c05704729fae981de8893215c06b64962db0bdb87158ee7b190eaf2"
+    root_url "https://github.com/canac/homebrew-tap/releases/download/kaw-0.1.4_1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a09cdd86ed9f06c14f917b9b41343d896cfcaa0f45fbb2f25205c407cd988271"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "65820211b49c764a77e24744188c71dbefd55aa085e34ec1fd5f658288b4195f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "20864741b4f482b13136abbab8922ceabda38f1d2bea1b17ec8a8c46ccaede96"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7c5fc7f3597e4d131c9d040fbaa6151271d76321d6a33645552ef28c03fe7ea6"
   end
 
   depends_on "rust" => :build
